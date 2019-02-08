d03a2ac3003a2515eda4bca686e64c8556dc162c
Abhishek <abhivaidya@gmail.com>
2019-02-08 17:23:33 +0530

    All build files

---
+++ roundtheworld/precache-manifest.82f30e1ae3b068b872a11edefdeae7d3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5a0964b530c93110e89",
+    "url": "game.4bdd41d284b85ac66a6e.js"
+  },
+  {
+    "revision": "ca6cdf4ef73e4b00114a",
+    "url": "vendors.ec979e7c6669a3ff598c.js"
+  },
+  {
+    "revision": "da7d9638e6a17e35a7858c2806c6e6e1",
+    "url": "index.html"
+  },
+  {
+    "revision": "2fd37c200e64a8b50e4908614ddec5e8",
+    "url": "sw.js"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "assets/icons/favicon.ico"
+  },
+  {
+    "revision": "e0e682b1c4fae3658c723b3b368a239a",
+    "url": "assets/icons/icons-192.png"
+  },
+  {
+    "revision": "b4c0f4323258e2f5e8d5107c12f772a1",
+    "url": "assets/img/IG-Logo_white.png"
+  },
+  {
+    "revision": "87a13dd52db0f12fa4b68f055deabe7f",
+    "url": "assets/img/player.png"
+  },
+  {
+    "revision": "09c3591bdcf81a611a1b07063866ae8d",
+    "url": "assets/sounds/lowThreeTone.ogg"
+  },
+  {
+    "revision": "5c59ed4cba5cad45447daf9b93ddf32c",
+    "url": "assets/img/particle.png"
+  },
+  {
+    "revision": "cc16bc8fa142e0ac5f7c3607cd2adb04",
+    "url": "assets/img/spike.png"
+  },
+  {
+    "revision": "4f716428b2831e9330a024ffb09d37cd",
+    "url": "assets/sounds/phaseJump1.ogg"
+  },
+  {
+    "revision": "70016cbebf0d6fc1007750016303e588",
+    "url": "assets/sounds/phaseJump2.ogg"
+  },
+  {
+    "revision": "1cc00c97cccfb293227d4cf7bb2c410d",
+    "url": "assets/sounds/phaseJump3.ogg"
+  },
+  {
+    "revision": "3b98e1cea6e8c3d776265148adf9aaf8",
+    "url": "assets/icons/icons-512.png"
+  },
+  {
+    "revision": "295ed7ee50248aeb3f976854b4ee654a",
+    "url": "assets/img/bigcircle_.png"
+  },
+  {
+    "revision": "65c9559057f64dfa75bab5819191f18a",
+    "url": "assets/img/mask.png"
+  },
+  {
+    "revision": "7387e8f2f0b2055a845df4d4b8ffa2c0",
+    "url": "assets/img/bigcircle.png"
+  },
+  {
+    "revision": "6859b36ad85eedd08bbbbb254c6f919d",
+    "url": "assets/sounds/bgmusic.mp3"
+  }
+];
---
+++ roundtheworld/sw.js
@@ -0,0 +1,4 @@
+importScripts("precache-manifest.82f30e1ae3b068b872a11edefdeae7d3.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
